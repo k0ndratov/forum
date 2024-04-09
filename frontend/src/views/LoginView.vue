ec7f21b20d99a949161c92fc62f41e11591f9610
Andrei Kondratov <peckondratov@gmail.com>
2024-04-09 13:45:04 -0700

    feat: frontend for login page

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,9 @@
+<template>
+  <form>
+    <label for="email">Email<input type="email" /></label>
+    <label for="email">Password<input type="password" /></label>
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<script setup></script>
